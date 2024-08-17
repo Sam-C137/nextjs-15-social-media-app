5892397c593f62a8c5575da089377b5cc926ec66
Sackah <yeitsam5@gmail.com>
2024-08-17 15:06:52 +0000

    fix: logo href

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -6,10 +6,7 @@ export default function Navbar() {
     return (
         <header className="sticky top-0 z-10 bg-card shadow-sm">
             <div className="mx-auto flex max-w-7xl flex-wrap items-center justify-center gap-5 px-5 py-3">
-                <Link
-                    href="/public"
-                    className="text-2xl font-bold text-primary"
-                >
+                <Link href="/" className="text-2xl font-bold text-primary">
                     bugbook
                 </Link>
                 <SearchInput />
